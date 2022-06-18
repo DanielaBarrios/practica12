7fd9b2ac4603a14a0c5557a49c2043b731509f60
DanielaBarrios <103336142+DanielaBarrios@users.noreply.github.com>
2022-06-18 16:38:32 -0500

    Update practica12.py

--- practica12.py
+++ practica12.py
@@ -3,3 +3,76 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math as ma
 import cv2 #opencv
+
+##practica 12
+# import the necessary packages
+from skimage.exposure import rescale_intensity
+import numpy as np
+import argparse
+import cv2
+
+def convolve(image, kernel):
+	(iH, iW) = image.shape[:2]
+	(kH, kW) = kernel.shape[:2]
+	pad = (kW - 1) // 2
+	image = cv2.copyMakeBorder(image, pad, pad, pad, pad,cv2.BORDER_REPLICATE)
+	output = np.zeros((iH, iW), dtype="float32")
+	for y in np.arange(pad, iH + pad):
+		for x in np.arange(pad, iW + pad):
+			roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
+			k = (roi * kernel).sum()
+			output[y - pad, x - pad] = k
+	output = rescale_intensity(output, in_range=(0, 255))
+	output = (output * 255).astype("uint8")
+	return output
+
+
+smallBlur = np.ones((7, 7), dtype="float") * (1.0 / (7 * 7))
+largeBlur = np.ones((21, 21), dtype="float") * (1.0 / (21 * 21))
+# Kernel sharpen
+sharpen = np.array((
+	[0, -1, 0],
+	[-1, 5, -1],
+	[0, -1, 0]), dtype="int")
+# Kernel laplacian
+laplacian = np.array((
+	[0, 1, 0],
+	[1, -4, 1],
+	[0, 1, 0]), dtype="int")
+# Kernel solve x
+sobelX = np.array((
+	[-1, 0, 1],
+	[-2, 0, 2],
+	[-1, 0, 1]), dtype="int")
+# Kernel solve y
+sobelY = np.array((
+	[-1, -2, -1],
+	[0, 0, 0],
+	[1, 2, 1]), dtype="int")
+# construct the kernel bank, a list of kernels we're going
+# to apply using both our custom `convole` function and
+# OpenCV's `filter2D` function
+kernelBank = (
+	("small_blur", smallBlur),
+	("large_blur", largeBlur),
+	("sharpen", sharpen),
+	("laplacian", laplacian),
+	("sobel_x", sobelX),
+	("sobel_y", sobelY)
+)
+# load the input image and convert it to grayscale
+image = cv2.imread("gato.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# loop over the kernels
+for (kernelName, kernel) in kernelBank:
+
+	print("[INFO] applying {} kernel".format(kernelName))
+	convoleOutput = convolve(gray, kernel)
+	opencvOutput = cv2.filter2D(gray, -1, kernel)
+	# show the output images
+	cv2.imshow("original", gray)
+	
+	cv2.imshow("{} - convole".format(kernelName), convoleOutput)
+	cv2.imshow("{} - opencv".format(kernelName), opencvOutput)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
